afad674b82244e4390715ef7a1385a3c850665df
lux-sales <sales_l@hotmail.com>
2022-04-19 14:00:44 -0300

    almost finishing the hot recipe card, just have to add margin in the paragraph

--- src/components/HotRecipeCard/index.tsx
+++ src/components/HotRecipeCard/index.tsx
@@ -1,36 +1,46 @@
 import React from "react";
 import { Container } from "./styles";
+import TimerIcon from "@mui/icons-material/Timer";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
 function HotRecipeCard() {
   return (
     <Container>
-      <span>Hot Recipes</span>
+      <div className="hot-label">
+        <span>Hot Recipes</span>
+      </div>
       <h1>Spicy Delicious chicken wings</h1>
-      <span>
+      <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
         labore voluptate, molestiae aliquid dolorem voluptates officiis
         dignissimos!
-      </span>
-      <div>
-        <div>
-          <span>Minutes</span>
+      </p>
+      <div className="info-div">
+        <div className="info-card">
+          <TimerIcon />
+          <span>30 Minutes</span>
         </div>
-        <div>
+        <div className="info-card">
+          <RestaurantIcon />
           <span>Chicken</span>
         </div>
       </div>
-      <div>
-        <div>
-          <img src="" alt="" />
+      <footer>
+        <div className="user-card">
+          <img
+            src="https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg"
+            alt="user-avatar"
+          />
           <div>
             <span>John Smith</span>
             <span>15 March 2022</span>
           </div>
-          <div>
-            <span>View Recipes</span>
-
-          </div>
         </div>
-      </div>
+        <div className="view-recipes">
+          <span>View Recipes</span>
+          <PlayCircleFilledWhiteIcon />
+        </div>
+      </footer>
     </Container>
   );
 }
--- src/components/HotRecipeCard/styles.ts
+++ src/components/HotRecipeCard/styles.ts
@@ -9,6 +9,105 @@ background-size: 50% 100%;
 margin: 40px 50px 160px 80px;
 border-radius: 45px 0px 0px 45px;
 padding: 40px 10px 40px 50px;
+
+h1{
+  width: 450px;
+  font-size: 50px;
+  margin: 23px 0px;
+  font-weight: bold;
+}
+p{
+  width: 550px;
+  color: #5C6466;
+  margin-bottom: 35px;
+}
+
+footer{
+  display: flex;
+  justify-content: space-between;
+  width: 450px;
+}
+
+.hot-label{
+  display: flex;
+  background-color: #fff;
+  border-radius: 25px;
+  width: 140px;
+  height: 50px;
+  align-items: center;
+  justify-content: right;
+  span{
+    font-weight: bold;
+    margin-right: 15px;
+    font-size: 14px;
+  }
+}
+
+.info-div{
+display: flex;
+width: 225px;
+justify-content: space-between;
+margin-bottom: 200px;
+}
+
+.info-card{
+  display: flex;
+  background-color: #DBEDF1;
+  border-radius: 15px;
+  font-size: 14px;
+  align-items: center;
+  justify-content: center;
+  padding: 2px 5px;
+  svg,span{
+    margin: 3px;
+  }
+  span{
+    color: #5C6466;
+  }
+}
+
+.user-card{
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  img{
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+  div{
+    display: flex;
+    flex-direction: column;
+    margin-left: 15px;
+    span:first-child{
+      font-weight: bold;
+    }
+    span+span{
+      color: #5C6466;
+    }
+
+  }
+}
+
+.view-recipes{
+    display:flex;
+    background-color: #000000;
+    align-items: center;
+    justify-content: center;
+    border-radius: 15px;
+    span{
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+      color: #fff;
+      font-weight: bold;
+      margin: 20px 15px 20px 30px;
+    }
+    svg{
+      color: #fff;
+      margin-right: 30px;
+    }
+  }
 `
 
 
